891d75a45a3cd21f4cf1ae0895786c435c4d743e
Mogbonjubola Favour <105045421+favour-22@users.noreply.github.com>
2023-05-07 18:51:06 +0100

    validating

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -21,3 +21,16 @@ def hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     
     return hashed_password
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks if a plaintext password matches a hashed password.
+
+    Args:
+        hashed_password: The hashed password as a byte string.
+        password: The plaintext password to check.
+
+    Returns:
+        True if the plaintext password matches the hashed password, False otherwise.
+    """
+    # Hash the plaintext password with the same salt as the hashed password
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
