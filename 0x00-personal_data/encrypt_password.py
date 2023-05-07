d0410f3e4d04171e9ed3b91f353f46afa91b201e
Mogbonjubola Favour <105045421+favour-22@users.noreply.github.com>
2023-05-07 18:27:37 +0100

    hash

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,18 @@
+import bcrypt
 
+def hash_password(password: str) -> bytes:
+    """Hashes a password using bcrypt.
+
+    Args:
+        password: The password to hash.
+
+    Returns:
+        A salted, hashed password as a byte string.
+    """
+    # Generate a random salt
+    salt = bcrypt.gensalt()
+
+    # Hash the password with the salt
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    return hashed_password
